ca807ade2b750dfb9ec22d5757aadae3390164b2
hiraoku.shinichi <hiraoku.shinichi@PC-05032.local>
2021-06-07 19:46:55 +0900

    emotion追加

--- pages/posts/firstPost.tsx
+++ pages/posts/firstPost.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import styled from "@emotion/styled";
+
+const StyledDiv = styled.div`
+  color: red;
+`;
 
 export default function FirstPost() {
   return (
     <>
+      <StyledDiv>Red</StyledDiv>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
