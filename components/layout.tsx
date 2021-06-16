54b6538c70d3066d1b4495b017a13402f744a7df
平奥真一 <hiraoku.shinichi@MacBook-Pro-hiraoku.local>
2021-06-16 21:14:14 +0900

    globals.css 追加

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,13 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  max-width: 36rem;
+  padding: 0 1rem;
+  margin: 3rem auto 6rem;
+`;
+
 function Layout({ children }) {
-  return <div>{children}</div>;
+  return <Container>{children}</Container>;
 }
 
 export default Layout;
